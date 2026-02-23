d44d788ff22d85b63c5267234493095267573742
gabriel_wandele <gabriel.merchant06@gmail.com>
2026-02-23 17:35:14 -0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+const firebaseConfig = {
+
+apiKey: "SUA_API_KEY",
+
+authDomain: "SEU_AUTH_DOMAIN",
+
+projectId: "SEU_PROJECT_ID",
+
+storageBucket: "SEU_STORAGE",
+
+messagingSenderId: "SEU_SENDER_ID",
+
+appId: "SEU_APP_ID"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
